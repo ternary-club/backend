018faa7af0490893f001fa6cf12073e68e8fc571
thzoid <thiagozoid@gmail.com>
2021-09-01 21:38:58 -0300

    Add version checking

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+// Initialization
+func main() {
+	// New Gin instance
+	r := gin.Default()
+
+	// Setup API
+	SetupCORS(r)
+	SetupRoutes(r)
+
+	// Start engine
+	r.Run()
+}
